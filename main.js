f6d78de586dc99c666bb0f4ae3d7cfc082aa6df5
VrindaTyagi123 <83289238+VrindaTyagi123@users.noreply.github.com>
2021-06-11 21:38:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-function setDate() {
-    document.getElementById('date').innerHTML = new Date().toDateString();
+function one(){
+  window.alert("Historically, elephants were classified into two species, the African (Loxodonta africana) and Asian (Elephas maximus) elephants. However, genetic research has provided new insight to elephant relatedness and taxonomic classification at the subspecies level. The African elephant (Loxodonta africana) originated in Africa, about 1.5 million years ago. Today, the African elephant is the largest living land animal. The Asian elephant (Elephas maximus) originated in Africa and migrated to Asia, where the species resides today.")
+}
+
+
+function two(){
+  window.alert("The botanical name of Crocodile is Crocodylinae. The genus Crocodylus — that of the 'true' crocodiles (pictured) — seems to have originated in Australasia, rather than in Africa as is commonly thought. Jamie Oaks at the University of Kansas in Lawrence analysed DNA-sequence data from 76 individuals across 23 crocodylian species.")
+}
+
+
+function three(){
+  window.alert("The polar bear's Latin name, Ursus maritimus, means sea bear. It's an apt name for this majestic species, which spends much of its life in, around, or on the ocean–predominantly on the sea ice. Genetic studies suggested that between 111 and 166 thousand years ago, a group of brown bears, possibly from Ireland, split off from their kin. In a blink of geological time, they adapted to the cold of the Arctic, and became the polar bears we know and worry about.")
+}
+
+function four(){
+  window.alert("Ostrich, (Struthio camelus), large flightless bird found only in open country in Africa. The largest living bird, an adult male may be 2.75 metres (about 9 feet) tall—almost half of its height is neck—and weigh more than 150 kg (330 pounds); the female is somewhat smaller. Common ostriches formerly occupied Africa north and south of the Sahara, East Africa, Africa south of the rainforest belt, and much of Asia Minor. Today common ostriches prefer open land and are native to the savannas and Sahel of Africa, both north and south of the equatorial forest zone.")
+}
+
+function five(){
+  window.alert("The botanical name of Whale is Cetacea. Cetaceans (whales, dolphins, and porpoises) are an order of mammals that originated about 50 million years ago in the Eocene epoch. Even though all modern cetaceans are obligate aquatic mammals, early cetaceans were amphibious, and their ancestors were terrestrial artiodactyls, similar to small deer.")
 }
